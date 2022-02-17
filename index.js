975bd15ff79c758c3580ae8492008bfbf5a1237e
angelique_rosin <angie1979.capucine@yahoo.fr>
2022-02-17 14:54:29 +0100

    ajout fichier js pour la navbar

---
+++ index.js
@@ -0,0 +1,13 @@
+//*******Apparition de la barre de navigation au scroll */
+
+const navigation = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+
+  if (window.scrollY > 250) {
+    navigation.style.top = 0;
+  }else{
+      navigation.style.top = "-65px"
+  }
+});
