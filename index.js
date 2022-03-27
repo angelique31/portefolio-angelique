17210ced73421a988a064f313b8e59ed5bb6ff42
angelique_rosin <angie1979.capucine@yahoo.fr>
2022-03-27 22:17:04 +0200

    modification du responsive

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //*******Apparition de la barre de navigation au scroll */
 
-// const navigation = document.querySelector("nav");
+const navigation = document.querySelector("nav");
 
-// window.addEventListener("scroll", () => {
-//   console.log(window.scrollY);
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
 
-//   if (window.scrollY > 250) {
-//     navigation.style.top = 0;
-//   }else{
-//       navigation.style.top = "-65px"
-//   }
-// });
+  if (window.scrollY > 250) {
+    navigation.style.top = 0;
+  }else{
+      navigation.style.top = "-65px"
+  }
+});
 
 
